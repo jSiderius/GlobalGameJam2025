e10f4e0a421f73b84012b7d789c956d74137e4e4
Joshua <jsiderius0@gmail.com>
2025-02-01 19:18:12 -0500

    Began work on level editor. Next: standardize sprite sizes in editor

---
+++ src/scenes/level editor/scripts/item_texture.gd
@@ -0,0 +1,15 @@
+extends TextureRect
+
+@export var this_scene:PackedScene
+
+@onready var object_cursor = get_node("/root/level_editor/editor_object")
+@onready var cursor_sprite = object_cursor.get_node("Sprite")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass 
+
+func _item_clicked(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed: 
+		object_cursor.current_item = this_scene
+		cursor_sprite.texture = texture
